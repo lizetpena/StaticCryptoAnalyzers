5a73d8265efc4ac79da41012741acb7bdd36a55a
Lizet Pena De Sola <lipenade@microsoft.com>
2018-03-08 17:33:24 -0600

    nuget package added to the ConsoleDotNet46 project. change in project file, program class instantiates DES, TripleDES and MD5 ciphers

--- ConsoleDotNet46/Program.cs
+++ ConsoleDotNet46/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace ConsoleDotNet46
 {
@@ -7,6 +9,15 @@ namespace ConsoleDotNet46
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var cipher = TripleDES.Create();
+            cipher.CreateEncryptor();
+
+            var cipher2 = DES.Create();
+            cipher2.CreateEncryptor();
+
+            var hash = MD5.Create();
+            var data = hash.ComputeHash(Encoding.UTF8.GetBytes("string to hash"));
+
         }
     }
 }
