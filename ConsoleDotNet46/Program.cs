56f1da37f7175bcec31c4fb07692373c50a83f40
Lizet Pena De Sola <lipenade@microsoft.com>
2018-03-08 17:38:18 -0600

    MD5 is not detected as a weak hashing function.

--- ConsoleDotNet46/Program.cs
+++ ConsoleDotNet46/Program.cs
@@ -4,8 +4,13 @@ using System.Text;
 
 namespace ConsoleDotNet46
 {
+    /// <summary>
+    /// This console class is only meant to test the Microsoft.NetFramework.Analyzers nuget package to see if it detects the use of weak crypto
+    /// algorithms.
+    /// </summary>
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
--- ConsoleNET452/Program.cs
+++ ConsoleNET452/Program.cs
@@ -7,6 +7,10 @@ using System.Security.Cryptography;
 
 namespace ConsoleNET452
 {
+    /// <summary>
+    /// This console class is only meant to test the Microsoft.NetFramework.Analyzers nuget package to see if it detects the use of weak crypto
+    /// algorithms.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
