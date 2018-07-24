fc057b0794b79faf6139dfbfe173be1711f1893f
Lizet Pena De Sola <lipenade@microsoft.com>
2018-07-24 08:14:24 -0500

    New comment on class summary

--- ConsoleDotNet46/Program.cs
+++ ConsoleDotNet46/Program.cs
@@ -5,7 +5,8 @@ using System.Text;
 namespace ConsoleDotNet46
 {
     /// <summary>
-    /// This Program class is only meant to test the Microsoft.NetFramework.Analyzers nuget package to see if it detects the use of weak crypto
+    /// This Program class is only meant to test the Microsoft.NetFramework.Analyzers and the FxCop nuget packages
+    /// This Nuget packages detect the use of weak crypto
     /// algorithms.
     /// </summary>
     class Program
--- ConsoleNET452/Program.cs
+++ ConsoleNET452/Program.cs
@@ -8,7 +8,8 @@ using System.Security.Cryptography;
 namespace ConsoleNET452
 {
     /// <summary>
-    /// This Program class is only meant to test the Microsoft.NetFramework.Analyzers nuget package to see if it detects the use of weak crypto
+    /// This Program class is only meant to test the Microsoft.NetFramework.Analyzers and the FxCop nuget packages
+    /// This Nuget packages detect the use of weak crypto
     /// algorithms.
     /// </summary>
     class Program
